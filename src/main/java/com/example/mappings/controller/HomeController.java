4e4404413b23b6667f64ff4bf59129e054305f82
Manju Vasavi Chinni <chinnidattasai@gmail.com>
2025-01-16 21:33:36 +0530

    added controller

---
+++ src/main/java/com/example/mappings/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.mappings.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+	
+	@GetMapping("/api/hello")
+	public String hello() {
+		return "hello mappings";
+	}
+
+}
