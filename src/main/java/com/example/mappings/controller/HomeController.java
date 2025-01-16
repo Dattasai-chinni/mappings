ca77ed80e192cd20c270b8841ca704640970607f
Manju Vasavi Chinni <chinnidattasai@gmail.com>
2025-01-16 22:53:10 +0530

    removed comment

--- src/main/java/com/example/mappings/controller/HomeController.java
+++ src/main/java/com/example/mappings/controller/HomeController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HomeController {
 	
-	//getting the  initial page
+
 	@GetMapping("/api/hello")
 	public String hello() {
 		return "hello mappings"; //returning string
